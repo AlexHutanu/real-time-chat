eaefde0df1ea972129100df8959fdc22cc2cad27
AlexHutanu <valexhutanu@gmail.com>
2023-12-11 14:47:29 +0200

    initialize websockets

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { socket, WebsocketProvider } from './contexts/WebsocketContext';
+import { Websocket } from './components/Websocket';
+
+function App() {
+  return (
+    <WebsocketProvider value={socket}>
+      <Websocket />
+    </WebsocketProvider>
+  );
+}
+
+export default App;
